c9bc41038686c323ccae2ed7c9ff271571889c8c
Samuel Leutner <samuel.leutner@proton.me>
2025-03-04 20:52:34 -0300

    project base

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem-vindo ao TP 2 de Java de Samuel!");
+
+        int x;
+
+        do {
+            System.out.println("\nEscolha um exercício para rodar:");
+            System.out.print("Digite a opção desejada: ");
+            System.out.println("0 - Sair");
+            for (int i = 1; i <= 12; i++) {
+                System.out.println(i + " - Exercício " + i);
+            }
+
+            x = sc.nextInt();
+            switch (x) {
+                case 0 -> System.exit(0);
+                case 1 -> ex1();
+                case 2 -> ex2();
+                case 3 -> ex3();
+                case 4 -> ex4();
+                case 5 -> ex5();
+                case 6 -> ex6();
+                case 7 -> ex7();
+                case 8 -> ex8();
+                case 9 -> ex9();
+                case 10 -> ex10();
+                case 11 -> ex11();
+                case 12 -> ex12();
+                default -> System.out.println("Valor inválido! Tente novamente.");
+            }
+        } while (x != 0);
+
+        sc.close();
+    }
+
+    public static void ex1() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 1:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex2() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 2:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex3() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 3:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex4() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 4:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex5() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 5:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex6() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 6:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex7() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 7:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex8() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 8:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex9() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 9:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex10() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 10:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex11() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 11:");
+        System.out.println("\n==============\n");
+    }
+
+    public static void ex12() {
+        System.out.println("\n==============\n");
+        System.out.println("Exercício 12:");
+        System.out.println("\n==============\n");
+    }
+}
